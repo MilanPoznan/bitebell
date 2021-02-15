f34a2dee45f1f86f9ad7c7837bc7c85a9d4f6d05
Igor <igor.grujic93@gmail.com>
2021-02-15 15:52:41 +0100

    Data, logic and hero component done

---
+++ src/components/Hero.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types'
+import './Hero.scss'
+
+export default function Hero({heroSection}) {
+
+  console.log(heroSection)
+  const {heroTitle, heroSubtitle} = heroSection
+
+  return (
+    <div className="header">
+    <div className="container hero">
+      <h1>{heroTitle}</h1>
+      <div dangerouslySetInnerHTML={{__html: heroSubtitle}}></div>
+    </div>
+    </div>
+  )
+}
+
+Hero.propTypes = {
+  heroSection: PropTypes.object
+}
