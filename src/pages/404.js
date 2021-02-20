77297291d4ddc6fcc89e68a8c7d6833af50d5cea
Milan Poznan <milan@jl.is>
2021-02-20 11:42:42 +0100

    refactore layout

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,6 @@
 import React from "react"
 import { graphql } from "gatsby"
 
-import Layout from "../components/Layout"
 import SEO from "../components/seo"
 
 const NotFoundPage = ({ data, location }) => {
