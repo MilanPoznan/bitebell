6ee7b118811ccf33faf1eaea907a61dc4fce1bba
Igor <igor.grujic93@gmail.com>
2021-02-21 15:57:00 +0100

    All markup done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,47 @@
-import React from 'react';
+import React from 'react'
+import FooterMenuItems from './FooterMenuItems'
+import HeartIcon from '../img/heart-icon.svg'
+import './Footer.scss'
 
-export default function Footer() {
+export default function Footer({currLangFooter, language}) {
 
+  const { menuItems: { nodes } } = currLangFooter;
+
+  const isEn = language === 'en' && true
+
+  const footerText = {
+    copyright: isEn ? '© Copyright' : '© Sva prava zadržana',
+    build: isEn ? 'Built with ' : 'Napravljeno s ljubavlju ',
+    city: isEn ? ' in Belgrade' : ' u Beogradu'
+  }
+
+  const  menuItemParent = nodes.filter(node => node.parentDatabaseId === 0)
+  
   return (
-    <div>Footer</div>
+    <div className="footer">
+      <div className="container">
+        <div className="footer-nav">
+          {
+            menuItemParent.map((node, index) => {
+
+              return (
+                <div className="footer-nav-list footer__container" key={index}>
+                  <p>{node.label}</p>
+                  <FooterMenuItems childItems={node}/>
+                </div>
+              )
+            })
+          }
+        </div>
+        <div className="footer-inner footer__copyright-wrapper">
+          <p>{footerText.copyright} {(new Date().getFullYear())} Bitebell Technologies.</p>
+          <div className="footer__copyright">
+            <p>{footerText.build} </p>
+            <img src={HeartIcon}/>
+            <p> {footerText.city}</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
--- src/templates/page-template.js
+++ src/templates/page-template.js
@@ -12,14 +12,15 @@ export default function page({ data }) {
 
   const { allWpPage: { nodes }, allWpMenu: { menus }, wp: { optionsPage: { options: { logo } } } } = data;
   const { title, language, translations, homepage_sections: { aboutSection, demoSection, heroSection, mapSection, posSection, sponsorsSection } } = nodes[0];
-  console.log(menus)
+
   const menuPosition = language.slug === 'sr' ? "MENU_1" : "MENU_1___EN";
   const currLangMenu = menus.filter(menu => menu.locations[0] === menuPosition)
 
-  // const footerPosition = menus.filter(menu => menu.locations[0] ===  ) 
+  const footerPosition = language.slug === 'sr' ? "FOOTER_MENU" : "FOOTER_MENU___EN";
+  const currLangFooter = menus.filter(menu => menu.locations[0] ===  footerPosition)
 
   return (
-    <Layout language={language.slug} title={title} translations={translations} currLangMenu={currLangMenu[0]} logo={logo}>
+    <Layout language={language.slug} title={title} translations={translations} currLangMenu={currLangMenu[0]} logo={logo} currLangFooter={currLangFooter[0]}>
       <Hero heroSection={heroSection}/>
       <PosComponent posSection={posSection}/>
       <MapComponent mapSection={mapSection}/>
