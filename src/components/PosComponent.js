9bc7a9473fb52ccc10bea7f8f1e9625bcb366f00
Milan Poznan <milan@jl.is>
2021-02-21 18:44:52 +0100

    add checking for image

--- src/components/PosComponent.js
+++ src/components/PosComponent.js
@@ -4,9 +4,9 @@ import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 import './PosComponent.scss'
 
-export default function PosComponent({posSection}) {
-  
-  const {link, partnersLogoIcons, partnersRepeater, subtitle, tabelIcon, text, title} = posSection
+export default function PosComponent({ posSection }) {
+
+  const { link, partnersLogoIcons, partnersRepeater, subtitle, tabelIcon, text, title } = posSection
 
   return (
     <div className="container">
@@ -17,7 +17,7 @@ export default function PosComponent({posSection}) {
             <h5>{subtitle}</h5>
           </div>
         </div>
-        <Img fluid={partnersLogoIcons.localFile.childImageSharp.fluid} />
+        {partnersLogoIcons && <Img fluid={partnersLogoIcons.localFile.childImageSharp.fluid} />}
         <div className="swiper-container swiper-table swiper-container-initialized swiper-container-vertical swiper-container-ios">
           <div className="swiper-wrapper" id="swiper-wrapper-2277adbd582785eb">
             {
@@ -25,7 +25,7 @@ export default function PosComponent({posSection}) {
                 return (
                   <div className="swiper-slide" key={index}>
                     <div className={`col-1 ${slide.color}-color`}>
-                        <span className="color-mark"></span>
+                      <span className="color-mark"></span>
                     </div>
                     <div className={`col-sm-2 col-3 ${slide.color}-color`}>
                       <p>{slide.name}</p>
@@ -39,23 +39,23 @@ export default function PosComponent({posSection}) {
                     <div className="col-2">
                       <p>{slide.price}</p>
                     </div>
-                </div>
+                  </div>
                 )
               })
             }
           </div>
         </div>
-        <Img fluid={tabelIcon.localFile.childImageSharp.fluid} className="table-icons"/>
+        <Img fluid={tabelIcon.localFile.childImageSharp.fluid} className="table-icons" />
         <div className="col-lg-6 aos-init aos-animate">
           <div className="text-box">
             <div className="mobile-hide">
               <h2>{title}</h2>
               <h5>{subtitle}</h5>
             </div>
-            <div dangerouslySetInnerHTML={{__html: text}}></div>
+            <div dangerouslySetInnerHTML={{ __html: text }}></div>
             <Link to={link.url} className="button button-lg connect-integrations-en">{link.title}</Link>
           </div>
-        </div>  
+        </div>
       </div>
     </div>
   )
