1e45b404ede031da1660fe4fa68a565ac9667978
Milan Poznan <milan@jl.is>
2021-02-22 12:55:25 +0100

    add table

--- src/components/PosComponent.js
+++ src/components/PosComponent.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import Img from 'gatsby-image'
 import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
@@ -10,20 +10,20 @@ export default function PosComponent({ posSection }) {
 
   const [rotateIndex, setRotateIndex] = useState(0)
   const [yCalc, setYCalc] = useState(0)
+  const [isGoesUp, setIsGoesUp] = useState(false)
 
   useEffect(() => {
-    let timer = null;
-    
-    setTimeout(() => {
-      console.log('dsfadga')
-      partnersRepeater.unshift(partnersRepeater.pop());
-      setRotateIndex( rotateIndex => rotateIndex++)
+    const interval = setInterval(() => {
+      if (Math.abs(rotateIndex) === partnersRepeater.length - 6) {
+        setIsGoesUp(true)
+      } else if (rotateIndex + 1 === 0) {
+        setIsGoesUp(false)
+      }
+      isGoesUp ? setRotateIndex(rotateIndex + 1) : setRotateIndex(rotateIndex - 1)
       setYCalc(65 * rotateIndex)
-      console.log(yCalc)
-
-    }, 500);
-    return () => clearTimeout(timer)
-  })
+    }, 3000);
+    return () => clearInterval(interval);
+  });
 
   return (
     <div className="container">
@@ -37,11 +37,11 @@ export default function PosComponent({ posSection }) {
           </div>
           {partnersLogoIcons && <Img fluid={partnersLogoIcons.localFile.childImageSharp.fluid} />}
           <div className="pos__swiper-wrapper swiper-container swiper-table swiper-container-initialized swiper-container-vertical swiper-container-ios">
-            <div className="swiper-wrapper pos__swiper" id="swiper-wrapper-2277adbd582785eb">
+            <div className="swiper-wrapper pos__swiper" id="swiper-wrapper-2277adbd582785eb" style={{ transform: `translateY(${yCalc}px)`, transition: '0.5s' }}>
               {
                 partnersRepeater.map((slide, index) => {
                   return (
-                    <div className={`pos__slide pos__slide pos-${index}`} style={{ transform: `translateY(${yCalc}px)` }} key={index}>
+                    <div className={`pos__slide pos__slide pos-${index}`} key={index}>
                       <div className={`pos__slide-element pos__slide-element--${slide.color}`}></div>
                       <div className="pos__slide-name-wrapper">
                         <p className={`pos__slide-name--${slide.color}`}>{slide.name}</p>
@@ -61,7 +61,7 @@ export default function PosComponent({ posSection }) {
               }
             </div>
           </div>
-          {tabelIcon && <Img fluid={tabelIcon.localFile.childImageSharp.fluid} className="table-icons pos__table-icon"/>}
+          {tabelIcon && <Img fluid={tabelIcon.localFile.childImageSharp.fluid} className="table-icons pos__table-icon" />}
         </div>
         <div className="pos__text-container">
           <div className="text-box">
