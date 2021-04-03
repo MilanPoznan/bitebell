57e015017475022710b11522e96f5e2c65b102ef
Milan Poznan <milan@jl.is>
2021-04-03 11:01:25 +0200

    add cookie popup

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -48,20 +48,31 @@ const Layout = ({ title, seoTitle, metaDesc, currLangMenu, children, logo, langu
         <Footer currLangFooter={currLangFooter} language={language} />
       </footer>
       <CookieConsent
-        // disableStyles={true}
+        disableStyles={true}
         location="bottom"
-        // buttonClasses="btn-custom-class"
-        // containerClasses="container-custom-class"
-        // contentClasses="content-custom-class"
-        buttonText="Accept"
+        buttonClasses="cookie-popup-btn"
+        containerClasses="cookie-popup"
+        contentClasses="cookie-content"
+        buttonText={language === 'sr' ? "U redu" : "Accept"}
         enableDeclineButton={true}
         declineButtonText="Decline"
         cookieName="gatsby-gdpr-google-analytics"
-      // onAccept={() => {
-      //   Cookies.set("gatsby-gdpr-google-tagmanager", true)
-      // }}
+        onAccept={() => {
+          // Cookies.set("gatsby-gdpr-google-tagmanager", true)
+          // window[`ga-disable-UA-46011787-23`] = true
+
+        }}
+        onDecline={() => {
+          window[`ga-disable-UA-46011787-23`] = true
+
+        }}
       >
-        This site uses cookies ...
+        <svg width="41" height="41" viewBox="0 0 41 41"><g fill="none" fill-rule="evenodd"><path fill="#9B572B" d="M29.59 0c-4.235.005-8.103 2.404-9.991 6.195 7.645 1.346 13.63 7.333 14.975 14.978 4.647-2.307 7.094-7.512 5.905-12.563C39.29 3.56 34.779-.008 29.59 0z" opacity=".7"></path><path fill="#B9896A" d="M16.439 7.89C7.36 7.89 0 15.25 0 24.33c0 9.079 7.36 16.439 16.439 16.439s16.439-7.36 16.439-16.44c-.01-9.074-7.364-16.428-16.44-16.438z"></path><g fill="#864937" transform="translate(6.632 5.426)"><circle cx="12.059" cy="11.456" r="1.809"></circle><circle cx="18.088" cy="21.706" r="1.809"></circle><circle cx="9.647" cy="19.294" r="1.809"></circle><circle cx="1.809" cy="15.676" r="1.809"></circle><circle cx="6.632" cy="27.132" r="1.809"></circle><circle cx="25.926" cy="1.809" r="1.809"></circle></g></g></svg>
+        <p>
+          {language === 'sr'
+            ? 'Mi koristimo kolačiće u cilju unapređenja usluge'
+            : 'We are using cookies for user experience improving '}
+        </p>
       </CookieConsent>
     </div>
   )
