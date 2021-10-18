199b287bc324e8ed86013e9cc6d3d7097f3ef19f
Milan Poznan <milan@jl.is>
2021-10-18 13:40:43 +0200

    add filter by categories

--- src/components/FaqTemplate.js
+++ src/components/FaqTemplate.js
@@ -13,13 +13,21 @@ export default function FaqTemplate({ title, subtitle, accordionArray, demoSecti
 
   const selectRef = useRef(null)
 
+
   function setCategories() {
     const categories = accordionArray.reduce((acc, curr) => {
-      !acc.includes(curr.category) && acc.push(curr.category)
+      let allCategories = curr.category.split(',')
+      allCategories.forEach((item, index) => {
+        let preparedString = index !== 0 ? item.substring(1) : item
+        !acc.includes(preparedString) && acc.push(preparedString)
+      })
+
       return acc
     }, [])
+
     let categoryName = language === 'sr' ? 'sve' : 'all'
     categories.unshift(categoryName)
+
     return categories
   }
 
@@ -31,11 +39,10 @@ export default function FaqTemplate({ title, subtitle, accordionArray, demoSecti
 
   function filterArray() {
     if (currCategory === 'all') return accordionArray
-    const x = accordionArray.filter(item => item.category === currCategory)
-    return x
+    return accordionArray.filter(item => item.category.includes(currCategory))
   }
   function handleOnFiler(e, index) {
-    setCurrCategory(e.target.innerText.toLowerCase())
+    setCurrCategory(e.target.innerText)
     setActiveFiler(index)
   }
 
@@ -48,6 +55,7 @@ export default function FaqTemplate({ title, subtitle, accordionArray, demoSecti
   useEffect(() => {
     handleOnChangeInput()
   }, [inputVal])
+
   useEffect(() => {
     setaAcordionState(filterArray())
   }, [currCategory])
@@ -70,12 +78,10 @@ export default function FaqTemplate({ title, subtitle, accordionArray, demoSecti
 
         <CategoryMobileTitle>{language === 'sr' ? 'Kategorije' : 'Topic'}</CategoryMobileTitle>
         <SelectWrapper>
-          <SelectFilter onChange={(e) => setCurrCategory(e.target.value.toLocaleLowerCase())} ref={selectRef}>
+          <SelectFilter onChange={(e) => setCurrCategory(e.target.value)} ref={selectRef}>
             {options && options.map(item => <option key={item} value={item}>{item}</option>)}
           </SelectFilter>
-          <svg xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 0 24 24" width="18px" fill="#000000"
-            onClick={() => console.log(selectRef.current)}
-          >
+          <svg xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 0 24 24" width="18px" fill="#000000">
             <path d="M24 24H0V0h24v24z" fill="none" opacity=".87" />
             <path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6-1.41-1.41z" />
           </svg>
