ae0a2fe04c772828db70fa87bd01703e39212f9a
Milan Poznan <milan@jl.is>
2022-12-28 13:30:08 +0100

    add title

--- src/components/NewsSection.js
+++ src/components/NewsSection.js
@@ -3,15 +3,21 @@ import Img from 'gatsby-image'
 
 import './NewsSection.scss'
 
-export default function NewsSection({ newsData }) {
+export default function NewsSection({ newsData, language }) {
 
-  console.log(newsData)
+  const title = language === 'en' ? 'Featured on' : 'Objavili su nas'
+
   return (
-    <div className="news-section">{
-      newsData.map((item, index) => <a href={item.newsUrl} target="_blank" key={index}>
-        {item.newsImage && <div className="news-section__img"
-          style={{ backgroundImage: `url(${item.newsImage.localFile.url} )` }} />}
-      </a>)
-    }</div>
+    <div>
+      <h2 className='news-section__title'>{title}</h2>
+      <div className="news-section">
+        {
+          newsData.map((item, index) => <a href={item.newsUrl} target="_blank" key={index}>
+            {item.newsImage && <div className="news-section__img"
+              style={{ backgroundImage: `url(${item.newsImage.localFile.url} )` }} />}
+          </a>)
+        }</div>
+
+    </div>
   )
 }
